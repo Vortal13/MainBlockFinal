0923586966d1bc669853d8a56d9cdb33fcc750e3
Vortal13 <100987@bk.ru>
2023-07-27 17:07:48 +0300

    Добавление программы

---
+++ Task/Program.cs
@@ -0,0 +1,56 @@
+﻿
+
+int NumberOfElements(string[] fistArray)
+{
+    int count = 0;
+    foreach (string item in fistArray)
+    {
+        if (item.Length <= 3)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+void SelectedArray(string[] fistArray, string[] secondArray)
+{
+    int count = 0;
+    for (int i = 0; i < fistArray.Length; i++)
+    {
+        if (fistArray[i].Length <= 3)
+        {
+            secondArray[count] = fistArray[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {        
+        System.Console.Write($"{array[i]}; ");
+    }
+    System.Console.WriteLine();
+}
+
+System.Console.Write("Введите количество элементов массива:");
+int size = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+string[] fistArray = new string[size];
+if (size == 0) System.Console.Write("Такого массива не существует.");
+else
+{
+    for (int i = 0; i < size; i++)
+    {
+        System.Console.Write($"Введите элемент №{i}: ");
+        fistArray[i] = Console.ReadLine()!;
+    }
+    System.Console.WriteLine($"Ваш массив: ");
+    System.Console.WriteLine(string.Join("; ", fistArray));
+}
+string[] secondArray = new string[NumberOfElements(fistArray)];
+
+SelectedArray(fistArray, secondArray);
+System.Console.WriteLine("Получившийся массив:");
+PrintArray(secondArray);
